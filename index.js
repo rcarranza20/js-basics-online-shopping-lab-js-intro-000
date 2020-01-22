95f4fe5eb4ddf362a23a44a0059b26c7a3a2f239
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-22 16:48:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,29 +18,57 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  if (cart.length === 0) {
-    return "Your shopping cart is empty."
-  } else if (cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-  } else if (cart.length === 2) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-  } else {
-    var myString = "In your cart, you have "
-    for(var i = 0; i < cart.length - 1; i++) {
-      myString = myString + `${cart[i].itemName} at $${cart[i].itemPrice}, `
-    }
-    return myString + `and ${cart[cart.length - 1].itemName} at $${cart[cart.length - 1].itemPrice}.`
-  }
+if (cart.length === 0){
+return "Your shopping cart is empty.";
 }
-
+else if(cart.length === 1){
+  return "In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}."
+  }
+  else if (cart.length === 2){
+  return "In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}, and ${cart[1].itemName} at ${cart[1].itemPrice}."
+  }
+  else {
+  var = myString("In your cart, you have ")
+  for (i = 0; i <cart.length -1; i++) {
+  myString = myString + '${cart[i].itemName} at ${cart[i].itemPrice}'
+  }
+  return myString + 'and ${cart[cart.length-1].itemName} at ${cart[cart.length-1].itemPrice}.' 
+  }
+  
+  
 function total() {
-
+total = 0;
+for(i= 0; i < cart.length; i++){
+total += cart[i].itemPrice;}
+return total;
 }
 
 function removeFromCart(item) {
-  // write your code here
+for (i=0; i < cart.length; i++){
+splice.cart(i, 1);
+return cart
+}
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+if (typeof(cardNumber) === "undefined"){
+return "Sorry, we don;'t have a credit card on file for you."}
+else {
+cart = [];
+return 'Your total cost is $${total}, which will be charged to ${cardNumber}.'
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
